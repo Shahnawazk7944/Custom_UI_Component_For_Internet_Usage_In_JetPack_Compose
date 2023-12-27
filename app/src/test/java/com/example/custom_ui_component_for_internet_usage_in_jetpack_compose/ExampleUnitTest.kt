2fbd91ad727635d0d9a947f805b632554cdf11a2
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2023-12-27 19:58:53 +0530

    add to repo

---
+++ app/src/test/java/com/example/custom_ui_component_for_internet_usage_in_jetpack_compose/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.custom_ui_component_for_internet_usage_in_jetpack_compose
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Custom_UI_Component_For_Internet_Usage_In_JetPack_Compose"
+include(":app")
